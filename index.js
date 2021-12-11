37736f7f9bcdee08391df8e8670ea4a1eacd7a83
Chak Tang <CHAKL@hotmail.com>
2021-12-11 17:36:43 +0100

    Finished exercise 3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let persons = [
     }
 ]
 
+//Get all
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
@@ -41,13 +42,26 @@ app.get('/info', (request, response) => {
 
 })
 
+// Fetch single resource
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-
     const person = persons.find(p => p.id === id)
 
     if (person) {
         response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+app.delete('/api/persons/:id' , (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(person) {
+        persons = persons.filter(p => p.id !== id)
+        response.status(204).end()
     } else {
         response.status(404).end()
     }
